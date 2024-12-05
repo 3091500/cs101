8d92551980d12b5c49c62c67bedf68ef6dbda336
3091500 <165780488+3091500@users.noreply.github.com>
2024-12-05 15:48:23 +0800

    Create main5.c

---
+++ ex3/main5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+  printf("免費")；
+}
+else if (i >= 240){
+    print("240元")；
+}
+else{
+ if(ic30)[
+   int h=((i/30)+1)*30；
+   printf("%d 元" ，h)；
+ }
+   else{
+   printf("%d 元" ，i)；
+   }
+}
+return 0 ；
+}
+   
+   
