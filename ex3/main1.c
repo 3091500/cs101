c1ef38553d36880b3ec61402bcfa9acf2f99ac47
3091500 <165780488+3091500@users.noreply.github.com>
2024-12-12 03:09:10 +0800

    create main1.c

--- ex3/main1.c
+++ ex3/main1.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-int main(){
-    int i=8；
+#include <stdio.h>
+
+int main ()
+{
+    int i=10;
     if(i & (i-1)){
-    printf("false")；
+        printf("false");
     }
     else{
-    printf("true")
+    printf("true");
     }
-    return 0；
-    }
+    return 0;
+}
