c80a1c6f7fc05c788d282284016b6588550b1a7e
3091500 <165780488+3091500@users.noreply.github.com>
2024-12-12 02:51:44 +0800

    main5.c

--- ex3/main5.c
+++ ex3/main5.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-int main(){
-  printf("免費")；
-}
-else if (i >= 240){
-    print("240元")；
-}
-else{
- if(ic30)[
-   int h=((i/30)+1)*30；
-   printf("%d 元" ，h)；
- }
-   else{
-   printf("%d 元" ，i)；
-   }
-}
-return 0 ；
+
+int main ()
+{
+    int i;
+    scanf("%d",&i);
+    if(i<30)
+    {
+       printf("免費");
+    }
+    else if (i >= 240)
+    {
+        printf("240元");
+    }
+    else
+    {
+        if(i%30)
+        {
+            int h=((i/30)+1)*30;
+            printf("%d 元" ,h);
+        }
+        else
+        {
+            printf("%d 元" ,i);
+        }
+    }
+    return 0 ;
 }
    
    
