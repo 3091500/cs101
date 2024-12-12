183c3f078defad658a7f9af9c602067a2dd7b911
3091500 <165780488+3091500@users.noreply.github.com>
2024-12-12 12:54:55 +0800

    main2.c

--- ex3/main2.c
+++ ex3/main2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main(){
-    int i=2；
+    int i=2;
     if (i % 15==0){
-printf("love IU")；
+printf("love IU");
 }
 else if(i % 3==0){
-    printf("love")；
+    printf("love");
 }
 else if(i % 5==0){
-    printf("IU")；
+    printf("IU");
 }
 else{
-printf("%d\n"，i)；
+printf("%d\n",i);
 }
-return 0；
+return 0;
 }
   
